eb6177dc50bacf5d59720fc3bb9cb94999d4286e
Steven Fackler <sfackler@gmail.com>
2016-10-02 21:40:56 -0700

    Add some crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! A mapped diagnostic context (MDC) for use with the `log` crate.
+//!
+//! An MDC is a thread local map of strings used to make relevant information
+//! from a system available in its log messages. Logging crates such as
+//! [log4rs][log4rs] will retrieve values from the MDC for output.
+//!
+//! For example, a web server may process many requests simultaneously on
+//! different threads. Generating an ID for each request and storing it in the
+//! MDC makes it easy to partition log messages on a per-request basis.
+//!
+//! [log4rs]: https://crates.io/crates/log4rs
 use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::collections::HashMap;
