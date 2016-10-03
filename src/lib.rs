de8af615101ded34153412e05f3d99f508f4cecb
Steven Fackler <sfackler@gmail.com>
2016-10-02 22:38:02 -0700

    Add extend_scoped

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,8 @@
 //! ```
 //!
 //! [log4rs]: https://crates.io/crates/log4rs
+#![warn(missing_docs)]
+
 use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::collections::HashMap;
@@ -82,11 +84,44 @@ pub fn extend<K, V, I>(entries: I)
           V: Into<String>,
           I: IntoIterator<Item = (K, V)>
 {
+    MDC.with(|m| m.borrow_mut().extend(entries.into_iter().map(|(k, v)| (k.into(), v.into()))));
+}
+
+/// Extends the MDC with new entries in a scoped fashion.
+///
+/// When the returned guard falls out of scope, it will restore the old values corresponding to the
+/// keys.
+///
+/// # Examples
+///
+/// ```
+/// log_mdc::insert("foo", "a");
+///
+/// let guard = log_mdc::extend_scoped([("foo", "b"), ("fizz", "buzz")].iter().cloned());
+/// log_mdc::get("foo", |v| assert_eq!(Some("b"), v));
+/// log_mdc::get("fizz", |v| assert_eq!(Some("buzz"), v));
+///
+/// drop(guard);
+/// log_mdc::get("foo", |v| assert_eq!(Some("a"), v));
+/// log_mdc::get("fizz", |v| assert_eq!(None, v));
+/// ```
+pub fn extend_scoped<K, V, I>(entries: I) -> ExtendGuard
+    where K: Into<String>,
+          V: Into<String>,
+          I: IntoIterator<Item = (K, V)>
+{
     MDC.with(|m| {
         let mut m = m.borrow_mut();
-        for (key, value) in entries {
-            m.insert(key.into(), value.into());
-        }
+
+        let old_entries = entries.into_iter()
+            .map(|(k, v)| (k.into(), v.into()))
+            .map(|(k, v)| {
+                let v = m.insert(k.clone(), v);
+                (k, v)
+            })
+            .collect();
+
+        ExtendGuard(old_entries)
     })
 }
 
@@ -136,5 +171,23 @@ impl Drop for InsertGuard {
             Some(value) => insert(key, value),
             None => remove(&key),
         };
+    }
+}
+
+/// A guard objects which restores MDC entries when dropped.
+pub struct ExtendGuard(Vec<(String, Option<String>)>);
+
+impl Drop for ExtendGuard {
+    fn drop(&mut self) {
+        MDC.with(|m| {
+            let mut m = m.borrow_mut();
+
+            for (key, value) in self.0.drain(..) {
+                match value {
+                    Some(value) => m.insert(key, value),
+                    None => m.remove(&key),
+                };
+            }
+        })
     }
 }
