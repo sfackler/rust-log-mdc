b29f6dadab1776ff7f7e0a35bea7ad811b9bdb19
Steven Fackler <sfackler@gmail.com>
2016-10-03 20:41:28 -0700

    Release v0.1.0

---
+++ doc/v0.1.0/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["log_mdc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='log_mdc/struct.InsertGuard.html' title='log_mdc::InsertGuard'>InsertGuard</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='log_mdc/struct.ExtendGuard.html' title='log_mdc::ExtendGuard'>ExtendGuard</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
